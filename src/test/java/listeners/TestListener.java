968c2803486c3e91dce091ad66978718529438fb
SamiraKetabifard <samiraketabi.fard@gmail.com>
2026-09-08 19:31:34 +0330

    done

--- src/test/java/listeners/TestListener.java
+++ src/test/java/listeners/TestListener.java
@@ -7,24 +7,31 @@ import org.testng.ITestResult;
 
 import java.util.Map;
 
+
 public class TestListener implements ITestListener {
 
 
     @Override
     public void onTestFailure(ITestResult result) {
 
+
         BaseTest test =
                 (BaseTest) result.getInstance();
 
 
+
         String testName =
-                result.getMethod().getMethodName();
+                result.getMethod()
+                        .getMethodName();
+
 
 
         Object[] parameters =
                 result.getParameters();
 
 
+
+        // اگر DataProvider داشت
         if(parameters.length > 0 &&
                 parameters[0] instanceof Map) {
 
@@ -33,51 +40,26 @@ public class TestListener implements ITestListener {
                     (Map<String,Object>) parameters[0];
 
 
-            testName =
-                    data.get("testCase").toString();
+            if(data.containsKey("testCase")) {
 
+                testName =
+                        data.get("testCase")
+                                .toString();
+
+            }
         }
 
-
-        // ذخیره عکس داخل فولدر
+        // ذخیره PNG
         ScreenshotUtil.saveScreenshot(
                 test.getDriver(),
                 testName
         );
 
-
         // Attach به Allure
         ScreenshotUtil.attachScreenshot(
                 test.getDriver()
         );
+
+
     }
 }
-
-
-    //@Override
-    /*
-    public void onTestFailure(
-            ITestResult result) {
-
-
-        BaseTest test =
-                (BaseTest) result.getInstance();
-
-
-
-        // ذخیره PNG
-        ScreenshotUtil.saveScreenshot(
-                test.getDriver(),
-                result.getMethod()
-                        .getMethodName()
-        );
-
-
-
-        // Attach به Allure
-        ScreenshotUtil.attachScreenshot(
-                test.getDriver()
-        );
-
-    }*/
-
--- src/test/java/utils/ScreenshotUtil.java
+++ src/test/java/utils/ScreenshotUtil.java
@@ -3,14 +3,12 @@ package utils;
 import io.qameta.allure.Attachment;
 import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.*;
-
 import java.io.File;
 import java.io.IOException;
 
 public class ScreenshotUtil {
 
 
-    // ذخیره PNG داخل target/screenshots
     public static String saveScreenshot(
             WebDriver driver,
             String testName) {
@@ -24,7 +22,6 @@ public class ScreenshotUtil {
                 ts.getScreenshotAs(OutputType.FILE);
 
 
-
         File folder =
                 new File(
                         System.getProperty("user.dir")
@@ -35,14 +32,19 @@ public class ScreenshotUtil {
         if (!folder.exists()) {
             folder.mkdirs();
         }
+
+
         String fileName =
-                testName + "_" + System.currentTimeMillis() + ".png";
+                testName
+                        + "_"
+                        + System.currentTimeMillis()
+                        + ".png";
 
 
-        File destination = new File(
-                System.getProperty("user.dir")
-                        + "/screenshots/"
-                        + fileName);
+        File destination =
+                new File(folder, fileName);
+
+
         try {
 
             FileUtils.copyFile(
@@ -50,17 +52,17 @@ public class ScreenshotUtil {
                     destination
             );
 
-
         } catch (IOException e) {
 
             e.printStackTrace();
-
         }
 
 
         return destination.getAbsolutePath();
     }
-    // Attach کردن به Allure
+
+
+
     @Attachment(
             value = "Failure Screenshot",
             type = "image/png"
@@ -69,10 +71,9 @@ public class ScreenshotUtil {
             WebDriver driver) {
 
 
-        return ((TakesScreenshot) driver)
+        return ((TakesScreenshot)driver)
                 .getScreenshotAs(
                         OutputType.BYTES
                 );
     }
-
 }
